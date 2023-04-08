30897476e95b079e30750362a08d192d0cf8d931
刘林 <llworduseing@163.com>
2023-04-08 09:44:57 +0800

    修改hello word 报错问题

--- hello-word/test-cases.ts
+++ hello-word/test-cases.ts
@@ -1,4 +1,3 @@
 import type { Equal, Expect, NotAny } from "@type-challenges/utils";
 
-// 你需要使得如下这行不会抛出异常
-type test = Expect<Equal<HelloWorld, string>>;
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
